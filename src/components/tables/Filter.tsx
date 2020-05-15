a592818de3955e8737f8a2ccb6cda81a91d3376a
TraianAlex <victor_traian@yahoo.com>
2020-05-14 20:31:35 -0400

    comment issue

--- src/components/tables/Filter.tsx
+++ src/components/tables/Filter.tsx
@@ -8,6 +8,8 @@ import {
   useGroupBy,
   useExpanded,
   useRowSelect,
+  TableOptions,
+  UseFiltersOptions,
 } from "react-table";
 import matchSorter from "match-sorter";
 
@@ -323,7 +325,7 @@ fuzzyTextFilterFn.autoRemove = (val: any) => !val;
 //       filters,
 //       selectedRowIds,
 //     },
-//   } = useTable(
+//   } = useTable<Combined>(
 //     {
 //       columns,
 //       data,
@@ -519,51 +521,51 @@ fuzzyTextFilterFn.autoRemove = (val: any) => !val;
 //   );
 // }
 
-// Define a custom filter filter function!
-function filterGreaterThan(rows: any, id: any, filterValue: any) {
-  return rows.filter((row: any) => {
-    const rowValue = row.values[id];
-    return rowValue >= filterValue;
-  });
-}
+// // Define a custom filter filter function!
+// function filterGreaterThan(rows: any, id: any, filterValue: any) {
+//   return rows.filter((row: any) => {
+//     const rowValue = row.values[id];
+//     return rowValue >= filterValue;
+//   });
+// }
 
-// This is an autoRemove method on the filter function that
-// when given the new filter value and returns true, the filter
-// will be automatically removed. Normally this is just an undefined
-// check, but here, we want to remove the filter if it's not a number
-filterGreaterThan.autoRemove = (val: any) => typeof val !== "number";
+// // This is an autoRemove method on the filter function that
+// // when given the new filter value and returns true, the filter
+// // will be automatically removed. Normally this is just an undefined
+// // check, but here, we want to remove the filter if it's not a number
+// filterGreaterThan.autoRemove = (val: any) => typeof val !== "number";
 
-// This is a custom aggregator that
-// takes in an array of leaf values and
-// returns the rounded median
-function roundedMedian(leafValues: any) {
-  let min = leafValues[0] || 0;
-  let max = leafValues[0] || 0;
+// // This is a custom aggregator that
+// // takes in an array of leaf values and
+// // returns the rounded median
+// function roundedMedian(leafValues: any) {
+//   let min = leafValues[0] || 0;
+//   let max = leafValues[0] || 0;
 
-  leafValues.forEach((value: any) => {
-    min = Math.min(min, value);
-    max = Math.max(max, value);
-  });
+//   leafValues.forEach((value: any) => {
+//     min = Math.min(min, value);
+//     max = Math.max(max, value);
+//   });
 
-  return Math.round((min + max) / 2);
-}
+//   return Math.round((min + max) / 2);
+// }
 
-const IndeterminateCheckbox = React.forwardRef(
-  ({ indeterminate, ...rest }: { indeterminate: any; rest: any }, ref: any) => {
-    const defaultRef = React.useRef();
-    const resolvedRef = ref || defaultRef;
+// const IndeterminateCheckbox = React.forwardRef(
+//   ({ indeterminate, ...rest }: { indeterminate: any; rest: any }, ref: any) => {
+//     const defaultRef = React.useRef();
+//     const resolvedRef = ref || defaultRef;
 
-    React.useEffect(() => {
-      resolvedRef.current.indeterminate = indeterminate;
-    }, [resolvedRef, indeterminate]);
+//     React.useEffect(() => {
+//       resolvedRef.current.indeterminate = indeterminate;
+//     }, [resolvedRef, indeterminate]);
 
-    return (
-      <>
-        <input type="checkbox" ref={resolvedRef} {...rest} />
-      </>
-    );
-  }
-);
+//     return (
+//       <>
+//         <input type="checkbox" ref={resolvedRef} {...rest} />
+//       </>
+//     );
+//   }
+// );
 
 // function App() {
 //   const columns = React.useMemo(
