e36747a3dc9ce68b17204ea2d15766af97859016
Traian Alexandru <victor_traian@yahoo.com>
2022-09-24 23:09:22 -0400

    new set state

--- src/apps/todo-store/TodoApp.tsx
+++ src/apps/todo-store/TodoApp.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import TodosStandard from './todo-standard/Todos';
 import TodosSetState from './todo-setstate1/Todos';
+import TodosSetState2 from './todo-setstate2/Todos';
 import TodosDispatch from './todo-dispatch2/Todos';
 import TodosImmutable from './todo-immutable3/Todos';
 
@@ -14,12 +15,14 @@ export const TodoApp = () => {
         <ul>
           <li onClick={() => setPage('standard')}>Default</li>
           <li onClick={() => setPage('set-state')}>SetState</li>
+          <li onClick={() => setPage('set-state2')}>SetState2</li>
           <li onClick={() => setPage('dispatch')}>Dispatch</li>
           <li onClick={() => setPage('immutable')}>Immutable</li>
         </ul>
       </MenuStyled>
       {page === 'standard' && <TodosStandard />}
       {page === 'set-state' && <TodosSetState />}
+      {page === 'set-state2' && <TodosSetState2 />}
       {page === 'dispatch' && <TodosDispatch />}
       {page === 'immutable' && <TodosImmutable />}
     </>
