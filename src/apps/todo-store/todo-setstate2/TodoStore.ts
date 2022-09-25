bf7d7e45911fc601deb278100834a278982860b4
Traian Alexandru <victor_traian@yahoo.com>
2022-09-24 23:15:09 -0400

    move actions

--- src/apps/todo-store/todo-setstate2/TodoStore.ts
+++ src/apps/todo-store/todo-setstate2/TodoStore.ts
@@ -15,10 +15,36 @@ type TodosType = {
 
 const initialState: TodosType = {
   todos: [],
-  title: 'SetState',
+  title: 'SetState2',
   count: 0,
   user: '',
   list: [],
 };
 
 export const todoStore = createStore(initialState);
+
+const { getState, setState } = todoStore;
+
+setState({ user: 'Alex2' });
+
+export const todoAddHandler = (text: string) => {
+  setState({
+    todos: [...getState().todos, { id: Math.random().toString(), text: text }],
+    count: getState().count + 1,
+  });
+};
+
+export const createList = (text: string) => {
+  setState({
+    list: [...getState().list, text],
+  });
+};
+
+export const todoDeleteHandler = (todoId: string) => {
+  setState({
+    todos: [
+      ...getState().todos.filter((todo: TodosState) => todo.id !== todoId),
+    ],
+    count: getState().count - 1,
+  });
+};
