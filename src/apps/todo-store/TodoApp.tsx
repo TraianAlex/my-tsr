4d068ea45e61b84013021800727af1950ac40b37
Traian Alexandru <victor_traian@yahoo.com>
2022-10-22 09:41:47 -0400

    add new section

--- src/apps/todo-store/TodoApp.tsx
+++ src/apps/todo-store/TodoApp.tsx
@@ -4,6 +4,7 @@ import TodosStandard from './todo-standard/Todos';
 import TodosSetState from './todo-setstate1/Todos';
 import TodosSetState2 from './todo-setstate2/Todos';
 import TodosSetState3 from './todo-setstate3/Todos';
+import TodosSetState31 from './todo-setstate3.1/Todos';
 import TodosDispatch from './todo-dispatch/Todos';
 import TodosImmutable from './todo-immutable/Todos';
 
@@ -12,6 +13,7 @@ const PageView: any = {
   SETSTATE: TodosSetState,
   SETSTATE2: TodosSetState2,
   SETSTATE3: TodosSetState3,
+  SETSTATE31: TodosSetState31,
   DISPATCH: TodosDispatch,
   IMMUTABLE: TodosImmutable,
 };
@@ -29,6 +31,7 @@ export const TodoApp = () => {
           <li onClick={() => setPage('SETSTATE')}>SetState</li>
           <li onClick={() => setPage('SETSTATE2')}>SetState2</li>
           <li onClick={() => setPage('SETSTATE3')}>SetState3</li>
+          <li onClick={() => setPage('SETSTATE31')}>SetState3.1</li>
           <li onClick={() => setPage('DISPATCH')}>Dispatch</li>
           <li onClick={() => setPage('IMMUTABLE')}>Immutable</li>
         </ul>
---
+++ src/apps/todo-store/todo-setstate3.1/Count.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useStore } from './TodoStore';
+
+export const Count = () => {
+  const count = useStore('count');
+
+  console.log('render Count');
+
+  return (
+    <div style={{ width: '90%', margin: 'auto' }}>
+      <hr />
+      <div>{count} rows</div>
+    </div>
+  );
+};
