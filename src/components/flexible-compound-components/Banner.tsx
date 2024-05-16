ae999cb04c73617e2d7a108ce7821b1d90e170d5
Traian Alexandru <victor_traian@yahoo.com>
2024-05-16 15:20:51 -0400

    save

--- src/components/flexible-compound-components/Banner.tsx
+++ src/components/flexible-compound-components/Banner.tsx
@@ -1,29 +1,22 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
 interface Props {
   title: string;
   subtitle: string;
 }
 
-const Banner: React.FC<Props> = ({
-  title,
-  subtitle
-}): React.ReactElement => (
+const Banner: React.FC<Props> = ({ title, subtitle }): React.ReactElement => (
   <Wrapper>
-    <h1 data-hover=" 🚀 🎩">
-    {title}
-    
-    </h1>
+    <h1 data-hover=" 🚀 🎩">{title}</h1>
     <h2>
-    {subtitle}
-    <span data-hover="React TypeScript Template"/>
+      {subtitle}
+      <span data-hover="React TypeScript Template" />
     </h2>
   </Wrapper>
 );
 
 export default Banner;
-
 
 const Wrapper = styled.div`
   background: #333;
