c96f7edcfc836314c381a49f629a32e5e5988d73
Traian Alexandru <victor_traian@yahoo.com>
2026-09-24 19:12:50 -0400

    fix

--- craco.config.js
+++ craco.config.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack');
+
 module.exports = {
   style: {
     postcss: {
@@ -9,24 +11,43 @@ module.exports = {
       webpackConfig.resolve = webpackConfig.resolve || {};
       webpackConfig.resolve.fallback = {
         ...(webpackConfig.resolve.fallback || {}),
-        crypto: false,
-        stream: false,
-        buffer: false,
-        util: false,
-        assert: false,
-        http: false,
-        https: false,
-        os: false,
-        url: false,
-        zlib: false,
-        path: false,
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        buffer: require.resolve('buffer/'),
+        util: require.resolve('util/'),
+        assert: require.resolve('assert/'),
+        http: require.resolve('stream-http'),
+        https: require.resolve('https-browserify'),
+        os: require.resolve('os-browserify/browser'),
+        url: require.resolve('url/'),
+        zlib: require.resolve('browserify-zlib'),
+        path: require.resolve('path-browserify'),
+        querystring: require.resolve('querystring-es3'),
+        process: require.resolve('process/browser.js'),
+        vm: require.resolve('vm-browserify'),
         fs: false,
         net: false,
         tls: false,
         child_process: false,
-        querystring: false,
-        process: false,
       };
+
+      webpackConfig.resolve.alias = {
+        ...(webpackConfig.resolve.alias || {}),
+        'process/browser': require.resolve('process/browser.js'),
+      };
+
+      webpackConfig.plugins.push(
+        new webpack.ProvidePlugin({
+          process: 'process/browser.js',
+          Buffer: ['buffer', 'Buffer'],
+        }),
+      );
+
+      webpackConfig.ignoreWarnings = [
+        ...(webpackConfig.ignoreWarnings || []),
+        /Failed to parse source map/,
+      ];
+
       return webpackConfig;
     },
   },
